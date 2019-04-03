37f81143c5177d8e98cadc0eb7dd08b04d9c0be5
Emilie Laurent <emilie.laurent.1281@student.uu.se>
2019-04-03 20:42:16 +0200

    Adding canu batch script

---
+++ script/canu_batch_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 12:00:00
+#SBATCH -J canu_lf_genome_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user emilie.laurent@live.com
+
+# This script assembles a genome with canu
+# using the reads from fastq.gz files
+
+canu -p "lfer" genomeSize="2.6m" -d "/home/emla007/genome_analyses/data/DNA_data/assembly" stopOnReadQuality="false" correctedErrorRate="0.04" -pacbio-raw '~/genome_analyses/data/raw_data/DNA_raw_data/ERR2028*.fastq.gz'
