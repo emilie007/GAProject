aed6e6cb21af6fe759557adb66895162f35dfbef
Emilie Laurent <emla007@rackham2.uppmax.uu.se>
2019-04-04 14:23:50 +0200

    Changing Canu script again

--- script/canu_batch_script.sh
+++ script/canu_batch_script.sh
@@ -1,9 +1,9 @@
-canu#!/bin/bash -l
+#!/bin/bash -l
 
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 12:00:00
+#SBATCH -t 13:00:00
 #SBATCH -J canu_lf_genome_assembly
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user emilie.laurent@live.com
